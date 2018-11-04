961fde7c424f709f141683b7dea33ae4895e6531
xmonader <xmonader@gmail.com>
2018-11-04 19:55:33 +0200

    make sure to init result

--- src/loadenv.nim
+++ src/loadenv.nim
@@ -1,21 +1,16 @@
 import os, strutils, tables
 
 proc loadEnvFromFile*(path: string, sep='='): Table[string, string] =
-  var res = initTable[string, string]()
-  try:
-    let lines = readFile(path).splitlines()
-    for l in lines:
-      let lstripped = l.strip()
-      if lstripped == "" or lstripped.startsWith('#'):
-        continue
-      if lstripped.contains(sep):
-        let parts = lstripped.split(sep, maxsplit=1)
-        if len(parts) == 2:
-          res.add(parts[0].strip(), parts[1].strip())
-  except:
-    echo getCurrentExceptionMsg()
-
-  return res
+  result = initTable[string, string]()
+  let lines = readFile(path).splitlines()
+  for l in lines:
+    let lstripped = l.strip()
+    if lstripped == "" or lstripped.startsWith('#'):
+      continue
+    if lstripped.contains(sep):
+      let parts = lstripped.split(sep, maxsplit=1)
+      if len(parts) == 2:
+        result.add(parts[0].strip(), parts[1].strip())
 
 proc loadEnv*(path:string, sep='='): void = 
   for k,v in loadEnvFromFile(path, sep).pairs:
